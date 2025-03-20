cf61ebc835cbbde98e8125a8747ed3db14c57555
sanam shrestha <shresthasanam480@gmail.com>
2025-03-20 09:13:36 +0545

    wow

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,31 @@
+// import "./App.css";
+// import { Route } from "react-router-dom";
+// import ChatPage from "./Pages/ChatPage";
+// import HomePage from "./Pages/HomePage";
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Route path="/" component={HomePage} exact />
+//       <Route path="/chats" component={ChatPage} />
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import "./App.css";
-import { Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom"; // Import Switch
 import ChatPage from "./Pages/ChatPage";
 import HomePage from "./Pages/HomePage";
 
 function App() {
   return (
     <div className="App">
-      <Route path="/" component={HomePage} exact />
-      <Route path="/chats" component={ChatPage} />
+      <Switch>
+        <Route path="/" component={HomePage} exact />
+        <Route path="/chats" component={ChatPage} />
+      </Switch>
     </div>
   );
 }
